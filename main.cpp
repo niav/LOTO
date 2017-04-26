ec516b22731458b60b88ffca6efab05d75f77c58
mrIaroslav <niav@i.ua>
2017-04-26 19:52:35 +0200

    Changed rounding principle in the lucky array of doubles. Was: 5/3 =1; Now: 5/3=2;
    changed "ascending sort" function to template, a.i. lucky became an array of doubles.
    Clion has suggested me to use nullptr in the time().

--- main.cpp
+++ main.cpp
@@ -1,53 +1,57 @@
 #define _CRT_RAND_S
+
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
 #include <random>
 #include <ctime>
 #include <iomanip>
+
 using namespace std;
 
-void ascending_sort (int *arr, int grille_sz);
+void ascending_sort(int *arr, int grille_sz);
+
 void fill_arr(int *lot, int num);
-void check_duplicates (int *lot, int num);
+
+void check_duplicates(int *lot, int num);
+
 int elementReinit(int el);
 
-template <class T>
-void ascending_sort (T *arr, int grille_sz);
+template<class T>
+void ascending_sort(T *arr, int grille_sz);
 
 int main() {//LOTO sur FDJ.COM
 
-	int i;
+    int i;
     int cnt{0};
     time_t current = time(nullptr);
-    char* seedTime = ctime(&current);
+    char *seedTime = ctime(&current);
 
     fstream seedsFile;
     seedsFile.open("lotoResults.txt", ios::out | ios::app);
     if (seedsFile.is_open()) {
         seedsFile << seedTime;
-    }
-    else {
+    } else {
         cout << "ERROR. Could not create file!" << endl;
         cout << "Current results will not be stored on drive." << endl;
     }
 
     const int num = 5; //numbers quant(6 in LOTO)
-	const int lots = 3; //numbers of seeds +1 for the average results
-	int lot[num+1];
-    double lucky[num+1]{0};
+    const int lots = 3; //numbers of seeds +1 for the average results
+    int lot[num + 1];
+    double lucky[num + 1]{0};
 
 
-    while(cnt<lots) {
+    while (cnt < lots) {
         fill_arr(lot, num);
-        check_duplicates(lot,num);
+        check_duplicates(lot, num);
         ascending_sort(lot, num);
 
 
         cout << "seed #" << cnt << ':' << endl;
         seedsFile << "\nseed #" << cnt << ':' << endl;
         for (i = 0; i <= num; i++) {
-            lucky[i]+=lot[i];
+            lucky[i] += lot[i];
             cout << lot[i] << ' ';
             seedsFile << lot[i] << ' ';
         }
@@ -57,24 +61,24 @@ int main() {//LOTO sur FDJ.COM
     }
 
     for (i = 0; i <= num; i++) {
-        lucky[i]/=lots;
+        lucky[i] /= lots;
     }
     ascending_sort(lucky, num);
 
     cout << "Result array: " << '\n';
     seedsFile << "\nResult array:\n";
-	for (i = 0; i <= num; i++) {
+    for (i = 0; i <= num; i++) {
         seedsFile << lucky[i] << ' ';
-        cout << setprecision(2) << lucky[i]<< ' ';
+        cout << setprecision(2) << lucky[i] << ' ';
     }
 
     seedsFile << '\n' << "-------" << endl;
     seedsFile.close();
 
-	return 0;
+    return 0;
 }
 
-void ascending_sort (int *arr, int grille_sz) {
+void ascending_sort(int *arr, int grille_sz) {
 
     bool sorted{false};
 
@@ -94,8 +98,8 @@ void ascending_sort (int *arr, int grille_sz) {
 
 void fill_arr(int *lot, int num) {
     int j;
-    errno_t         err;
-    unsigned int    number;
+    errno_t err;
+    unsigned int number;
 
     // Fill 5 random integers in the range [ 1,49 ] (Grille 1).
     for (j = 0; j < num; j++) {
@@ -119,23 +123,23 @@ void fill_arr(int *lot, int num) {
         cout << "Error in random number generating" << endl;
     }
     lot[num] = (unsigned int) ((double) number /
-                                ((double) UINT_MAX + 1) * 10.0) + 1;
+                               ((double) UINT_MAX + 1) * 10.0) + 1;
 
 }
 
 int elementReinit(int el) {
-    errno_t         err;
-    unsigned int    number;
+    errno_t err;
+    unsigned int number;
 
     err = rand_s(&number);
     sleep((unsigned int) ((double) number /
-                              ((double) UINT_MAX + 1) * 6.0) + 1);
+                          ((double) UINT_MAX + 1) * 6.0) + 1);
 
     if (err != 0) {
-            cout << "Error in random number generating" << endl;
-        }
+        cout << "Error in random number generating" << endl;
+    }
     el = (unsigned int) ((double) number /
-                                 ((double) UINT_MAX + 1) * 49.0) + 1;
+                         ((double) UINT_MAX + 1) * 49.0) + 1;
 
     return el;
 }
@@ -154,7 +158,7 @@ void check_duplicates(int *lot, int num) {
                 }
             }
         }
-    } while(isDuplicates);
+    } while (isDuplicates);
 }
 
 template<class T>
